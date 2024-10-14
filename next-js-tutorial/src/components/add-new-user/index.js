fe002c46a58e9f77cca9ce699e3b409169f49cdd
erick njogu <njoguerick3@gmail.com>
2024-10-14 20:38:36 +0300

    add dialog popUp

---
+++ next-js-tutorial/src/components/add-new-user/index.js
@@ -0,0 +1,71 @@
+"use client";
+
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+function AddNewUser() {
+  const [openPopup, setOpenPopup] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setOpenPopup(true)}>Add User</Button>
+      <Dialog
+        open={openPopup}
+        onOpenChange={setOpenPopup}
+      >
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label
+                htmlFor="name"
+                className="text-right"
+              >
+                Name
+              </Label>
+              <Input
+                id="name"
+                defaultValue="Pedro Duarte"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label
+                htmlFor="username"
+                className="text-right"
+              >
+                Username
+              </Label>
+              <Input
+                id="username"
+                defaultValue="@peduarte"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddNewUser;

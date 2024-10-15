a4cf93d4b881f7fdd6b739811488ce5141597cea
erick njogu <njoguerick3@gmail.com>
2024-10-15 14:27:49 +0300

    edit

---
+++ next-js-tutorial/src/app/utils/index.js
@@ -0,0 +1,22 @@
+export const addNewUserForm = [
+  {
+    name: "firstName",
+    label: "First Name",
+    placeholder: "Enter First Name",
+  },
+  {
+    name: "lastName",
+    label: "Last Name",
+    placeholder: "Enter last Name",
+  },
+  {
+    name: "email",
+    label: "Email",
+    placeholder: "Enter Email",
+  },
+  {
+    name: "address",
+    label: "Address",
+    placeholder: "Enter Address",
+  },
+];
--- next-js-tutorial/src/components/add-new-user/index.js
+++ next-js-tutorial/src/components/add-new-user/index.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { addNewUserForm } from "@/app/utils";
 import { Button } from "../ui/button";
 import {
   Dialog,
@@ -26,38 +27,29 @@ function AddNewUser() {
       >
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
-            </DialogDescription>
+            <DialogTitle>Add New User</DialogTitle>
           </DialogHeader>
           <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label
-                htmlFor="name"
-                className="text-right"
+            {addNewUserForm.map((item, index) => (
+              <div
+                className="mb-5"
+                key={index}
               >
-                Name
-              </Label>
-              <Input
-                id="name"
-                defaultValue="Pedro Duarte"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label
-                htmlFor="username"
-                className="text-right"
-              >
-                Username
-              </Label>
-              <Input
-                id="username"
-                defaultValue="@peduarte"
-                className="col-span-3"
-              />
-            </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label
+                    htmlFor={item.name}
+                    className="text-right"
+                  >
+                    {item.label}
+                  </Label>
+                  <Input
+                    id={item.name}
+                    className="col-span-3"
+                    placeHolder={item.placeholder}
+                  />
+                </div>
+              </div>
+            ))}
           </div>
           <DialogFooter>
             <Button type="submit">Save changes</Button>
